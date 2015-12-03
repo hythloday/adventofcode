fb026126d7b80f9daa5217033cbd22298b9e67f2
R C James Harlow <git@james.harlow.name>
2015-12-03 14:15:22 +0000

    Day 2 part 2

--- src/main/scala/advent/Day2.scala
+++ src/main/scala/advent/Day2.scala
@@ -8,10 +8,15 @@ import Math._
 object Day2 extends App {
 
   val input = io.Source.fromInputStream(getClass.getClassLoader.getResourceAsStream("day2.txt")).getLines.toList
+  val boxes = input.map(Box.apply)
 
   case class Box(w: Int, h: Int, l: Int) {
+    def dims = List(w, h, l)
     def sides = List(l*w, w*h, h*l)
     def paper: Int = sides.map(_*2).sum + sides.min
+    def shortest = dims.sorted.take(2).map(_*2).sum
+    def bow = w*h*l
+    def ribbon = shortest + bow
   }
   object Box {
     def apply(s: String): Box = {
@@ -21,8 +26,13 @@ object Day2 extends App {
   }
 
   def part1 = {
-    input.map(Box.apply).map(_.paper).sum
+    boxes.map(_.paper).sum
+  }
+
+  def part2 = {
+    boxes.map(_.ribbon).sum
   }
 
   println(s"part1 = $part1")
+  println(s"part2 = $part2")
 }
