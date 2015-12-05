edaf7055fbf3ec69861560ec587244210a874b11
R C James Harlow <git@james.harlow.name>
2015-12-05 11:08:08 +0000

    Day 5 part 2

--- src/main/scala/advent/Day5.scala
+++ src/main/scala/advent/Day5.scala
@@ -1,6 +1,6 @@
 package advent
 
-import advent.Day3._
+import Math.abs
 
 /**
   * Created by james on 05/12/2015.
@@ -9,18 +9,35 @@ object Day5 extends App {
 
   val input = io.Source.fromInputStream(getClass.getClassLoader.getResourceAsStream("day5.txt")).getLines.toList
 
-  def isVowel(c: Char) = "aeiou" contains c
-  def prop1(s: String) = (s count isVowel) >= 3
+  def part1 = {
+    def isVowel(c: Char) = "aeiou" contains c
+    def prop1(s: String) = (s count isVowel) >= 3
 
-  def isDouble(pair: String) = pair.head == pair.last
-  def prop2(s: String) = s.sliding(2) exists isDouble
+    def isDouble(pair: String) = pair.head == pair.last
+    def prop2(s: String) = s.sliding(2) exists isDouble
 
-  def banned(pair: String) = Set("ab", "cd", "pq", "xy") contains pair
-  def prop3(s: String) = !(s.sliding(2) exists banned)
+    def banned(pair: String) = Set("ab", "cd", "pq", "xy") contains pair
+    def prop3(s: String) = !(s.sliding(2) exists banned)
 
-  def isNice(s: String) = prop1(s) && prop2(s) && prop3(s)
+    def isNice(s: String) = prop1(s) && prop2(s) && prop3(s)
 
-  def part1 = input count isNice
+    input count isNice
+  }
+
+  def part2 = {
+    def positionPairs(s: String): Map[String, List[Int]] = s.sliding(2).zipWithIndex.toList.groupBy(_._1).mapValues(_.map(_._2))
+    def nonAdjacentPair(pair: List[Int]) = abs(pair.head - pair.last) > 1
+    def nonAdjacent(xs: List[Int]) = xs.combinations(2) exists nonAdjacentPair
+    def prop1(s: String) = positionPairs(s).values exists nonAdjacent
+
+    def repeatsWith1LetterBetween(triple: String) = triple.head == triple.last
+    def prop2(s: String) = s.sliding(3) exists repeatsWith1LetterBetween
+
+    def isNice(s: String) = prop1(s) && prop2(s)
+
+    input count isNice
+  }
 
   println(s"part1 = $part1")
+  println(s"part2 = $part2")
 }
