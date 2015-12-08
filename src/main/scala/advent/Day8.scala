339da8c147ad901c30ad8f34133800cbbd62a8e7
R C James Harlow <git@james.harlow.name>
2015-12-08 15:37:07 +0000

    Slightly improve parser style

--- src/main/scala/advent/Day8.scala
+++ src/main/scala/advent/Day8.scala
@@ -11,12 +11,11 @@ object Day8 extends Advent with JavaTokenParsers {
   def quot = "\""
   def escBs = bs ~ bs
   def escQuot = bs ~ quot
-  def escChr = bs ~ "x[0-9a-f][0-9a-f]".r
-  def oneChr = "[a-z]".r
-  def char = escBs | escQuot | escChr | oneChr
-  def escapedLine = {
-    quot ~> (char +) <~ quot
-  }
+  def hex = "[0-9a-f]".r
+  def escChr = bs ~ "x" ~ hex ~ hex
+  def chr = "[a-z]".r
+  def char = escBs | escQuot | escChr | chr
+  def escapedLine = quot ~> (char +) <~ quot
 
   def part1 = input.map(_.length).sum - input.map(parse(escapedLine, _).get.size).sum
 
@@ -24,11 +23,9 @@ object Day8 extends Advent with JavaTokenParsers {
   def bsLen = bs ^^^ { 2 }
   def escQuotLen = escQuot ^^^ { 4 }
   def escChrLen = escChr ^^^ { 5 }
-  def charLen = oneChr ^^^ { 1 }
+  def charLen = chr ^^^ { 1 }
   def lineLen = quotLen | escChrLen | charLen | bsLen | escQuotLen
-  def escape(line: String) = {
-    parse(lineLen +, line).get.sum + 2
-  }
+  def escape(line: String) = parse(lineLen +, line).get.sum + 2
 
   def part2 = input.map(escape).sum - input.map(_.length).sum
 }
