4e3c3e24a9d4ab865ff76dfe6a76b23fd4621df7
R C James Harlow <git@james.harlow.name>
2015-12-13 06:33:17 +0000

    Day 13 part 1

---
+++ src/main/scala/advent/Day13.scala
@@ -0,0 +1,21 @@
+package advent
+
+/**
+  * Created by james on 13/12/2015.
+  */
+object Day13 extends Advent {
+
+  def happiness = (ident <~ "would") ~ ("lose" | "gain") ~ wholeNumber ~ ("happiness units by sitting next to" ~> ident <~ ".") ^^ {
+    case a~"lose"~n~b => (a, b) -> -n.toInt
+    case a~"gain"~n~b => (a, b) -> n.toInt
+  }
+
+  lazy val moods = input.map(parse(happiness, _).get).toMap
+  lazy val arrangements = moods.keys.flatMap{ case (a, b) => List(a, b) }.toList.permutations.toList
+  def happinessPair(ps: List[String]) = moods((ps.head, ps.last)) + moods((ps.last, ps.head))
+  def hScore(people: List[String]) =  people.sliding(2).map(happinessPair).sum + happinessPair(List(people.head, people.last))
+
+  def part1 = arrangements.map(hScore).max
+  def part2 = ???
+
+}
