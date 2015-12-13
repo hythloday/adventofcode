e2d071b90d81981f17019aaa8758e7ed60b70df9
R C James Harlow <git@james.harlow.name>
2015-12-13 06:59:22 +0000

    Use builtin Json parser rather than play's

--- build.sbt
+++ build.sbt
@@ -3,6 +3,5 @@ scalaVersion := "2.11.7"
 
 libraryDependencies ++= Seq(
   "org.scala-lang" % "scala-compiler" % "2.11.7",
-  "org.scala-lang" % "scala-reflect" % "2.11.7",
-  "com.typesafe.play" %% "play-json" % "2.4.4"
+  "org.scala-lang" % "scala-reflect" % "2.11.7"
 )
--- src/main/scala/advent/Day12.scala
+++ src/main/scala/advent/Day12.scala
@@ -1,28 +1,44 @@
 package advent
 
-import play.api.libs.json._
+//import play.api.libs.json._
+import scala.util.parsing.json._
 
 /**
   * Created by james on 12/12/2015.
   */
 object Day12 extends Advent {
 
-  lazy val json: JsValue = Json.parse(input.mkString)
+//  lazy val json: JsValue = Json.parse(input.toList.mkString)
+  lazy val Some(json) = JSON.parseRaw(input.toList.mkString)
 
-  def count1(v: JsValue): Long = v match {
-    case JsNumber(x) => x.toLong
-    case JsArray(a) => a.map(count1).sum
-    case JsObject(o) => o.values.map(count1).sum
-    case _ => 0L
+//  def count1(v: JsValue): Long = v match {
+//    case JsNumber(x) => x.toLong
+//    case JsArray(a) => a.map(count1).sum
+//    case JsObject(o) => o.values.map(count1).sum
+//    case _ => 0L
+//  }
+
+  def count(v: Any): Long = v match {
+    case x: Double => x.toLong
+    case JSONArray(xs) => xs.map(count).sum
+    case JSONObject(m) => m.values.map(count).sum
+    case _ => 0
   }
 
-  def count2(v: JsValue): Long = v match {
-    case JsNumber(x) => x.toLong
-    case JsArray(a) => a.map(count2).sum
-    case JsObject(o) if !(o.values.toList contains JsString("red")) => o.values.map(count2).sum
-    case _ => 0L
+//  def count2(v: JsValue): Long = v match {
+//    case JsNumber(x) => x.toLong
+//    case JsArray(a) => a.map(count2).sum
+//    case JsObject(o) if !(o.values.toList contains JsString("red")) => o.values.map(count2).sum
+//    case _ => 0L
+//  }
+
+  def count2(v: Any): Long = v match {
+    case x: Double => x.toLong
+    case JSONArray(xs) => xs.map(count2).sum
+    case JSONObject(m) if !(m.values.toList contains "red") => m.values.map(count2).sum
+    case _ => 0
   }
 
-  def part1 = count1(json)
+  def part1 = count(json)
   def part2 = count2(json)
 }
