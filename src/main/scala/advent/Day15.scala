b023aa2cc98103955bf6298f4b3c63d1b664fca4
R C James Harlow <git@james.harlow.name>
2015-12-15 09:23:47 +0000

    Day 15 part 1

---
+++ src/main/scala/advent/Day15.scala
@@ -0,0 +1,28 @@
+package advent
+
+/**
+  * Created by james on 15/12/2015.
+  */
+object Day15 extends Advent {
+
+  case class Ingredient(name: String, qualities: List[Long], calories: Long)
+
+  def anIngredient = ident ~ (": capacity" ~> wholeNumber) ~ (", durability" ~> wholeNumber) ~
+    (", flavor" ~> wholeNumber) ~ (", texture" ~> wholeNumber) ~ (", calories" ~> wholeNumber) ^^
+    { case n~c~d~f~t~cl => Ingredient(n, List(c.toInt, d.toInt, f.toInt, t.toInt), cl.toInt) }
+
+  lazy val ingredients = input.map(parse(anIngredient, _).get)
+
+  def bake(recipe: Map[Int, Ingredient]) = recipe.map { case (qty, ingredient) => ingredient.qualities.map(_*qty) }
+    .transpose.map(_.sum).map(Math.max(0, _)).product
+
+  def split4(i: Int) = for {
+    a <- 0 to i
+    b <- 0 to i - a
+    c <- 0 to i - (a + b)
+    d = i - (a + b + c)
+  } yield Seq(a, b, c, d)
+
+  def part1 = split4(100).map(qtys => qtys.zip(ingredients).toMap).map(bake).max
+  def part2 = ???
+}
