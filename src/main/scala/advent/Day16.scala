f40e267cdff1f92f19b02b49c7067185e8984027
R C James Harlow <git@james.harlow.name>
2015-12-16 16:02:22 +0000

    put Day 16 data into resources and make it a bit terser

--- src/main/scala/advent/Day16.scala
+++ src/main/scala/advent/Day16.scala
@@ -7,30 +7,22 @@ object Day16 extends Advent {
 
   type Sue = Map[String, Int]
 
-  def thing = "children" | "cats" | "samoyeds" | "pomeranians" | "akitas" | "vizslas" | "goldfish" | "trees" | "cars" | "perfumes"
-  def aSue = ("Sue" ~> wholeNumber <~ ":") ~
-    (thing <~ ":") ~ (wholeNumber <~ ",") ~
-    (thing <~ ":") ~ (wholeNumber <~ ",") ~
-    (thing <~ ":") ~ wholeNumber ^^ { case n~t1~q1~t2~q2~t3~q3 => n -> Map(t1 -> q1.toInt, t2 -> q2.toInt, t3 -> q3.toInt) }
+  def property = (ident <~ ":") ~ wholeNumber ^^ { case n~q => n -> q.toInt }
+  def aSue = ("Sue" ~> wholeNumber <~ ":") ~ repsep(property, ",") ^^ { case n~things => n -> things.toMap }
 
-  lazy val sues = input.map(parse(aSue, _).get).toMap
+  lazy val sues: Map[String, Map[String, Int]] = input.map(parse(aSue, _).get).toMap
 
-  lazy val cardSue = Map("children" -> 3, "cats" -> 7, "samoyeds" -> 2, "pomeranians" -> 3,
-    "akitas" -> 0, "vizslas" -> 0, "goldfish" -> 5, "trees" -> 3, "cars" -> 2, "perfumes" -> 1)
+  lazy val sender = parse(rep(property),
+    io.Source.fromURL(getClass.getClassLoader.getResource("day16-sender.txt")).mkString).get.toMap
 
-  def canBeSender(b: Sue) = cardSue.filter{ case (k, v) => b.keys.toSeq contains k } == b
+  def canBeSender(sender: Sue)(b: Sue) = sender.filter{ case (k, v) => b.get(k).isDefined } == b
 
-  def canBeSenderPart2(b: Sue) = {
-    val s = cardSue.filter{ case (k, v) => b.keys.toSeq contains k }
-    (s.keys.toList.sorted, s.keys.toList.sorted.map(s), s.keys.toList.sorted.map(b)).zipped.map {
-      case ("cats", sv, bv) => bv > sv
-      case ("trees", sv, bv) => bv > sv
-      case ("pomeranians", sv, bv) => bv < sv
-      case ("goldfish", sv, bv) => bv < sv
-      case (_, sv, bv) => sv == bv
-    }.forall(identity)
+  def canBeSenderPart2(sender: Sue)(b: Sue) = sender.keys.filter(b.contains).map(k => (k, sender(k), b(k))).forall {
+    case (thing, sv, bv) if Set("cats", "trees") contains thing           => bv > sv
+    case (thing, sv, bv) if Set("pomeranians", "goldfish") contains thing => bv < sv
+    case (_, sv, bv)                                                      => sv == bv
   }
 
-  def part1 = sues.collect{ case (n, sue) if canBeSender(sue) => n }
-  def part2 = sues.collect{ case (n, sue) if canBeSenderPart2(sue) => n }
+  def part1 = sues.collect{ case (n, sue) if canBeSender(sender)(sue) => n }
+  def part2 = sues.collect{ case (n, sue) if canBeSenderPart2(sender)(sue) => n }
 }
